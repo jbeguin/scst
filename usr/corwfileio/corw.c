4a242f5c153711b80749ade6a766883615679876
jbeguin <jmarie.beguin@gmail.com>
2021-08-23 20:48:43 +0200

    clean + oups

--- usr/corwfileio/corw.c
+++ usr/corwfileio/corw.c
@@ -1,25 +1,5 @@
 #include "corw.h"
 
-void load_block(struct corw_handler *h, int fuse_fd, int i) {
-    PRINT_INFO("load_block %d", i);
-    loff_t loff = ((long) i) * ((long) h->block_size);
-    char *buf = malloc(h->block_size);
-
-    /* READ */
-    lseek64(fuse_fd, loff, 0/*SEEK_SET*/);
-    int res = read(fuse_fd, buf, h->block_size);
-    if (res < 0) {
-		PRINT_ERROR("load_block(%d) returned %"PRId64" (errno %d)",
-			i, (uint64_t)res, errno);
-        return;
-    }
-
-    /* COPY to mmap */
-    memcpy(&(h->buf)[loff], buf, res);
-
-    free(buf);
-}
-
 struct corw_handler *corw_handler_create(char *corwfile, char *file, int64_t file_size, int block_size) {
     PRINT_INFO("Opening file %s", corwfile);
     PRINT_INFO("File size %"PRId64"", file_size);
@@ -75,65 +55,110 @@ struct corw_handler *corw_handler_create(char *corwfile, char *file, int64_t fil
     corwh->file_size = file_size;
     corwh->block_size = block_size;
     corwh->buf = buf;
-    corwh->nblocks = file_size / block_size;
-    if (file_size % block_size == 0) corwh->nblocks++;
+    corwh->nblocks = (file_size + block_size - 1) / block_size;
     size_t bitmaplen = 0;
     corwh->bitmap = bitmap_open_file(bitmapfn, corwh->nblocks, &bitmaplen, 0, 0);
     corwh->bitmap_fn = bitmapfn;
     corwh->bitmaplen = bitmaplen;
-	PRINT_INFO("corw_handler_create corwh->bitmaplen %d", (int) corwh->bitmaplen);
+	PRINT_INFO("corwh->bitmaplen %d", (int) corwh->bitmaplen);
+	PRINT_INFO("corwh->file_size %d", (int) corwh->file_size);
     
     return corwh;
 }
 
 void corw_handler_destroy(struct corw_handler *corwh) {
     bitmap_close_file(corwh->bitmap, corwh->bitmaplen);
+
 	int ret = msync(corwh->buf, corwh->file_size, MS_SYNC);
 	if (ret < 0)
 		PRINT_ERROR("msync corw_handler failed ret = %d", ret);
+
     ret = munmap(corwh->buf, corwh->file_size);
 	if (ret < 0)
 		PRINT_ERROR("munmap corw_handler ret = %d", ret);
+
     free(corwh->bitmap_fn);
     free(corwh);
 }
 
+ssize_t load_block(struct corw_handler *h, int fuse_fd, int i) {
+    PRINT_INFO("load_block %d", i);
+    loff_t loff = ((long) i) * ((long) h->block_size);
+    char *buf = malloc(h->block_size);
+
+    /* READ */
+    lseek64(fuse_fd, loff, 0/*SEEK_SET*/);
+    ssize_t res = read(fuse_fd, buf, h->block_size);
+    if (res < 0) {
+		PRINT_ERROR("load_block(%d) returned %"PRId64" (errno %d)",
+			i, (uint64_t)res, errno);
+        return res;
+    }
+
+    /* COPY to mmap */
+    memcpy(&(h->buf)[loff], buf, res);
+
+    free(buf);
+
+    return res;
+}
+
 void corw_handler_preload(struct corw_handler *h, int fuse_fd) {
-	PRINT_INFO("corw_handler_preload nblocks %d", h->nblocks);
+    int res = 0;
+	PRINT_INFO("nblocks %d", h->nblocks);
     for (int i = 0; i < h->nblocks; i++) {
         if (bitmap_test(h->bitmap, i)) {
-            load_block(h, fuse_fd, i);
+            res = load_block(h, fuse_fd, i);
+            if (res < 0) {
+                PRINT_ERROR("block %d fuse_fd %d", i, fuse_fd);
+                // et maintenant, qu'est ce qu'on fait ? (cf nemo)
+            }
         }
     }
 }
 
 ssize_t corw_handler_load_range(struct corw_handler *h, int fuse_fd, loff_t loff, int length) {
+    int res = 0;
     int start_block = loff / h->block_size;
     int end_block = (loff + length) / h->block_size;
-    TRACE_DBG("corw_handler_load_range start_block %d end_block %d", start_block, end_block);
+    TRACE_DBG("start_block %d end_block %d", start_block, end_block);
     for (int i = start_block; i <= end_block; i++) {
-        TRACE_DBG("corw_handler_load_range i %d bitmap_test(h->bitmap, i) %d", i, bitmap_test(h->bitmap, i));
+        TRACE_DBG("i %d bitmap_test(h->bitmap, i) %d", i, bitmap_test(h->bitmap, i));
         if (!bitmap_test(h->bitmap, i)) {
-            load_block(h, fuse_fd, i);
-            bitmap_on(h->bitmap, i);
+            res = load_block(h, fuse_fd, i);
+            if (res < 0) {
+                PRINT_ERROR("block %d fuse_fd %d", i, fuse_fd);
+            } else {
+                bitmap_on(h->bitmap, i);
+            }
         }
     }
     return 0;
 }
 
 ssize_t corw_handler_load_write_range(struct corw_handler *h, int fuse_fd, loff_t loff, int length) {
+    int res = 0;
     int start_block = loff / h->block_size;
     int end_block = (loff + length) / h->block_size;
-    TRACE_DBG("corw_handler_load_write_range start_block %d end_block %d", start_block, end_block);
+    TRACE_DBG("start_block %d end_block %d", start_block, end_block);
     if (!bitmap_test(h->bitmap, start_block)) {
-        load_block(h, fuse_fd, start_block);
-        bitmap_on(h->bitmap, start_block);
+        res = load_block(h, fuse_fd, start_block);
+        if (res < 0) {
+            PRINT_ERROR("block %d fuse_fd %d", start_block, fuse_fd);
+        } else {
+            bitmap_on(h->bitmap, start_block);
+        }
     }
     if (start_block != end_block) {
-        TRACE_DBG("corw_handler_load_write_range end_block %d bitmap_test(h->bitmap, end_block) %d", end_block, bitmap_test(h->bitmap, end_block));
+        TRACE_DBG("end_block %d bitmap_test(h->bitmap, end_block) %d", end_block, bitmap_test(h->bitmap, end_block));
         if (!bitmap_test(h->bitmap, end_block)) {
             load_block(h, fuse_fd, end_block);
-            bitmap_on(h->bitmap, end_block);
+            res = load_block(h, fuse_fd, end_block);
+            if (res < 0) {
+                PRINT_ERROR("block %d fuse_fd %d", end_block, fuse_fd);
+            } else {
+                bitmap_on(h->bitmap, end_block);
+            }
         }
     }
     return 0;
